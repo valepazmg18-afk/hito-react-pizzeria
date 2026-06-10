d9578ba3b4d21f8496f06aa2fa388eeeb0fa5c10
valentina muñoz <valepaz.mg18@gmail.com>
2026-06-10 17:41:17 -0400

    parte final de hito 8 react

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -10,7 +10,31 @@ export default function Cart() {
   const { cart, increase, decrease } = useContext(CartContext);
   const {total} = useContext(CartContext);
   console.log(cart);
-  const {token} = useContext(UserContext);
+  const {user} = useContext(UserContext);
+  const [message, setMessage] = useState("");
+  const handleCheckout = async () => {
+  try {
+    const response = await fetch("http://localhost:5000/api/checkouts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`
+      }
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      setMessage("¡Compra realizada con éxito!");
+    } else {
+      setMessage(data.error || "Error al realizar la compra");
+    }
+  } catch (error) {
+    console.error(error);
+    setMessage("Error de conexión");
+  }
+};
+  
   return (
     <>
       <div className="container bg-dark text-white my-5 py-5 text-center" style={{borderRadius:"10px"}}>
@@ -29,7 +53,8 @@ export default function Cart() {
           </div>
         </div>
         <h2>Total a pagar: ${total.toLocaleString("es-CL")}</h2>
-        <button className="btn btn-light" disabled={!token}>Finalizar Compra</button>
+        <button className="btn btn-light" disabled={!user} onClick={handleCheckout}> Finalizar Compra </button>
+        {message && (<p className="mt-3 text-success">{message}</p>)}
       </div>
     </>
   );
